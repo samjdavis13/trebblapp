329a529f58f8499fd3ad98a64944cd72ec9907e1
sd-forge <sd@weareforge.co>
2015-09-04 22:51:32 +1000

    finish off design

--- js/app.js
+++ js/app.js
@@ -22,3 +22,18 @@ $(window).resize(function(){
 });
 
 
+// Scrolling NAV
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 750);
+        return false;
+      }
+    }
+  });
+});
